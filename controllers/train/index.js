78c04a0e3648c5d8a69458b6205834b2eecff215
Tanya <duka.tanik@gmail.com>
2022-09-27 19:45:06 +0300

    add router deleteBook

--- controllers/train/index.js
+++ controllers/train/index.js
@@ -3,11 +3,13 @@ const addBookInTrain = require('./addBookInTrain');
 const updateStatusBook = require('./updateStatusBook');
 const addStatistic = require('./addStatistic');
 const addTrain = require('./addTrain');
+const deleteBookInTrain = require('./deleteBookInTrain');
 
 module.exports = {
     getTrain,
     addBookInTrain,
     updateStatusBook,
     addStatistic,
-    addTrain
+    addTrain,
+    deleteBookInTrain
 }
