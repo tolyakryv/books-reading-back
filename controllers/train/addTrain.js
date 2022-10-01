eccb69916490485c912991d925febf704981c4b2
Tanya <duka.tanik@gmail.com>
2022-10-01 20:14:59 +0300

    fix model statistic

--- controllers/train/addTrain.js
+++ controllers/train/addTrain.js
@@ -3,6 +3,7 @@ const { Train } = require('../../models/Train');
 
 const addTrain = async (req, res, next) => {
     const { _id } = req.user;
+    console.log(req.body);
 
     const result = await Train.create({ ...req.body, owner:_id });
   
