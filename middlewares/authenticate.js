07f074d9d2ca8808805946bee549072a01594007
Yuliia Shybyk <j7557500@gmail.com>
2022-10-01 19:43:46 +0300

    authenticate

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -13,7 +13,7 @@ const authenticate = async (req, _, next) => {
   try {
     const payload = jwt.verify(token, SECRET_KEY);
   
-    const user = await User.findById(payload.uid);
+    const user = await User.findById(payload.id);
     if (!user) {
       next(RequestError(401, "Not authorized"));
     }
