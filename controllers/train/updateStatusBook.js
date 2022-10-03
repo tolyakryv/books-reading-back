62608d537405618fb82c8082f076d9b2ce4edde3
Tanya <duka.tanik@gmail.com>
2022-10-03 19:51:13 +0300

    change route

--- controllers/train/updateStatusBook.js
+++ controllers/train/updateStatusBook.js
@@ -1,19 +1,22 @@
 const { Book } = require('../../models/Book')
-const {Train} = require('../../models/Train')
+const {Train } = require('../../models/Train')
 
 const updateStatusBook = async (req, res, next) => {
     const{_id}=req.user
     const { bookId } = req.params;
     const { status} = req.body;
     
-    await Book.findByIdAndUpdate(bookId, { status }, { new: true });
-    
-    const books=await Book.find();
-    const newBookInTrain = await books.filter(book => book.inTrain === true);
-
-    const newListTrain= await Train.findOneAndUpdate({ owner: _id }, {book:newBookInTrain},{new:true})
-    
-    res.json(newListTrain.book);
+    const oldBook=await Book.findOne({ _id:bookId, owner:_id });
+    const train = await Train.findOne({ owner: _id });
+  
+    for (const book of train.book) {
+        if (book.title === oldBook.title) {
+            book.status=status
+        }            
+    }
+    const newBooks=await Train.findOneAndUpdate({owner: _id},{book:train.book},{new:true})
+       
+    res.json(newBooks.book);
 }
 
 module.exports=updateStatusBook
